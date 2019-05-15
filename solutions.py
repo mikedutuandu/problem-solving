7189af378098e14ac7e128567bcf82e2e9424018
tuan.dinh <tuan.dinh@groovetechnology.com>
2019-05-15 17:14:27 +0700

    update

--- solutions.py
+++ solutions.py
@@ -75,3 +75,24 @@ rec = lib.get_recommend(3)
 
 browse(rec)
 
+# Find kth most occurred character
+
+def find_max_char(str_char):
+  l = {}
+  for i in str_char:
+    if i in l:
+      l[i] = l[i]+1
+    else:
+      l[i] = 1
+
+  found_char = None
+  found_char_number = 0
+  for k,v in l.items():
+    if found_char_number < v:
+      found_char_number = v
+      found_char = k
+
+  return found_char
+
+newstr = "asadddddasaadsd"
+print(find_max_char(newstr))
