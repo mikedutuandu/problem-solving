4fd8e2cf4cd3fcf7c9791c3e15d328a6eb541e79
mikedu <mikedutuandu@gmail.com>
2019-05-23 05:45:12 +0700

    update code

--- solutions.py
+++ solutions.py
@@ -227,7 +227,57 @@ lk1.add(node3)
 
 lk1.remove(node2)
 
+# Implement a queue using linkedlist
+# 1. implement front tro den vi tri dau, rear tro den vi tri cuoi cua queue
+# 2. enqueue: add vao vi tri cuoi (rear)
+# 3. dequeue: remove o vi tri dau tien (front)
 
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Queue:
+
+    def __init__(self):
+        self.front = self.rear = None
+
+    def isEmpty(self):
+        return self.front == None
+
+    # Method to add an item to the queue
+    def EnQueue(self, item):
+        temp = Node(item)
+
+        if self.rear == None:
+            self.front = self.rear = temp
+            return
+        self.rear.next = temp
+        self.rear = temp
+
+        # Method to remove an item from queue
+
+    def DeQueue(self):
+
+        if self.isEmpty():
+            return
+        temp = self.front
+        self.front = temp.next
+
+        if (self.front == None):
+            self.rear = None
+        return str(temp.data)
+
+q = Queue()
+q.EnQueue(10)
+q.EnQueue(20)
+q.DeQueue()
+q.DeQueue()
+q.EnQueue(30)
+q.EnQueue(40)
+q.EnQueue(50)
 
 
 
